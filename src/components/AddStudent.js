b66d49e4f2f669ddfc051b69cd4b67e0694f60ea
Sajan Kc <sajan1996kc@gmail.com>
2020-11-10 21:23:24 +0545

    delete funcanality added

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -28,6 +28,7 @@ const AddStudent = () => {
     await fetch("/addstudent", {
       method: "POST",
       headers: {
+        Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify(studentData),
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,71 +1,90 @@
 import React, { useState, useEffect } from "react";
-import { Container, Table } from "reactstrap";
+import { Button, Container, Table } from "reactstrap";
 import logo from "../logo.svg";
 
 const Student = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [students, setStudents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [students, setStudents] = useState([]);
 
-	const getStudent = async () => {
-		const response = await fetch("/student");
-		const result = await response.json();
-		setStudents(result);
-		setIsLoading(false);
-		return result;
-	};
+  const getStudent = async () => {
+    const response = await fetch("/student");
+    const result = await response.json();
+    setStudents(result);
+    setIsLoading(false);
+    return result;
+  };
 
-	useEffect(() => {
-		getStudent();
-	}, []);
-
-	// Populating table with students data
-	let studentData = students.map((student) => {
-		return (
-			<tr key={student.id}>
-				<th scope="row">{student.id}</th>
-				<td>{student.rollNo}</td>
-				<td>{student.fname}</td>
-				<td>{student.lname}</td>
-				<td>{student.gender}</td>
-				<td>{student.address}</td>
-				<td>{student.email}</td>
-				<td>{student.phone}</td>
-			</tr>
-		);
-	});
-
-	if (isLoading)
-		return (
-			<div className="loading">
-				<header className="loading__header">
-					<img src={logo} className="loading__logo" alt="logo" />
-                    <p>Loading... Please wait</p>
-				</header>
-			</div>
-		);
-	else {
-		return (
-			<Container>
-				<div className="navfixer"></div>
-				<h2>Student Information</h2>
-				<Table bordered hover responsive>
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>RollNo</th>
-							<th>First Name</th>
-							<th>Last Name</th>
-							<th>Address</th>
-							<th>Gender</th>
-							<th>Email</th>
-							<th>PhoneNo</th>
-						</tr>
-					</thead>
-					<tbody>{studentData}</tbody>
-				</Table>
-			</Container>
-		);
+  const deleteStudent = async (id) => {
+    if (window.confirm("Are you sure want to delete ?")) {
+      await fetch("/student/" + id, {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
 	}
+	getStudent();
+  };
+
+  useEffect(() => {
+    getStudent();
+  }, []);
+
+  // Populating table with students data
+  let studentData = students.map((student) => {
+    return (
+      <tr key={student.id}>
+        <th scope="row">{student.id}</th>
+        <td>{student.rollNo}</td>
+        <td>{student.fname}</td>
+        <td>{student.lname}</td>
+        <td>{student.address}</td>
+        <td>{student.gender}</td>
+        <td>{student.email}</td>
+        <td>{student.phone}</td>
+        <td>
+          <Button color="danger" onClick={() => deleteStudent(student.id)}>
+            X
+          </Button>{" "}
+        </td>
+      </tr>
+    );
+  });
+
+  if (isLoading)
+    return (
+      <div className="loading">
+        <header className="loading__header">
+          <img src={logo} className="loading__logo" alt="logo" />
+          <p>Loading... Please wait</p>
+        </header>
+      </div>
+    );
+  else {
+    return (
+      <Container>
+        <div className="navfixer"></div>
+        <h2>Student Information</h2>
+        <Table bordered hover responsive>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>RollNo</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Address</th>
+              <th>Gender</th>
+              <th>Email</th>
+              <th>PhoneNo</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>{studentData}</tbody>
+        </Table>
+      </Container>
+    );
+  }
 };
 
 export default Student;
